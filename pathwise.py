fab195e7ae92eedde94db0bf8487a8b587a4b134
Seyfal Sultanov <ssulta24@uic.edu>
2023-03-20 03:11:17 -0500

    modified:   pathwise.py

--- pathwise.py
+++ pathwise.py
@@ -1,43 +1,45 @@
-# openai.organization = "org-tUKmFHP7000FSOLkMus2M2B3"
+        # self.kc2_button = QtWidgets.QRadioButton('Summarize (KC2)')
+        # self.kc3_button = QtWidgets.QRadioButton('Questioning (KC3)')
+        # self.kc4_button = QtWidgets.QRadioButton('Vocabulary Support (KC4)')
+        # self.soc1_button = QtWidgets.QRadioButton('Robot Self Disclosure (SOC1)')
+        # self.soc2_button = QtWidgets.QRadioButton('Recall Part Interactions (SOC2)')
+        # self.soc3_button = QtWidgets.QRadioButton('Memory and Adaptation (SOC3)')
+        # self.soc4_button = QtWidgets.QRadioButton('Emotional Response (SOC4)')
+        # self.ic1_button = QtWidgets.QRadioButton('Value (IC1)')
+        # self.ic2_button = QtWidgets.QRadioButton('Belongingness (IC2)')
 
-import openai
-import tkinter as tk
-from tkinter import ttk
+        # self.button_group.addButton(self.kc2_button)
+        # self.button_group.addButton(self.kc3_button)
+        # self.button_group.addButton(self.kc4_button)
+        # self.button_group.addButton(self.soc1_button)
+        # self.button_group.addButton(self.soc2_button)
+        # self.button_group.addButton(self.soc3_button)
+        # self.button_group.addButton(self.soc4_button)
+        # self.button_group.addButton(self.ic1_button)
+        # self.button_group.addButton(self.ic2_button)
 
-openai.api_key = "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"
+        # vbox1.addWidget(self.kc2_button)
+        # vbox1.addWidget(self.kc3_button)
+        # vbox1.addWidget(self.kc4_button)
+        # vbox1.addWidget(self.soc1_button)
+        # vbox1.addWidget(self.soc2_button)
+        # vbox1.addWidget(self.soc3_button)
+        # vbox1.addWidget(self.soc4_button)
+        # vbox1.addWidget(self.ic1_button)
+        # vbox1.addWidget(self.ic2_button)
 
-def generate_text(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    return response.choices[0].text.strip()
-
-def on_submit():
-    prompt = user_input.get()
-    response = generate_text(prompt)
-    response_label.config(text=response)
-
-root = tk.Tk()
-root.title("OpenAI Python App")
-
-frame = ttk.Frame(root, padding="10")
-frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-
-user_input = tk.StringVar()
-input_label = ttk.Label(frame, text="Enter your input:")
-input_label.grid(row=0, column=0, sticky=(tk.W, tk.E))
-input_entry = ttk.Entry(frame, textvariable=user_input, width=40)
-input_entry.grid(row=0, column=1, sticky=(tk.W, tk.E))
-
-submit_button = ttk.Button(frame, text="Submit", command=on_submit)
-submit_button.grid(row=1, column=1, sticky=(tk.W, tk.E))
-
-response_label = ttk.Label(frame, text="", wraplength=300)
-response_label.grid(row=2, column=0, columnspan=2, sticky=(tk.W, tk.E))
-
-root.mainloop()
+        # prompt = f"Text: {user_input}\n\nComment Type: KC1\n\nAudience: first graders\n\n"
+        # prompt += "Explanation:\nA KC1 comment is a prediction comment that links relevant clues from within the text. "
+        # prompt += "It is based on scientific content information the child will encounter on the current page "
+        # prompt += "or within the next few pages of the book. The purpose of a KC1 comment is to help the child "
+        # prompt += "make connections between different pieces of information and understand what might happen next.\n\n"
+        # prompt += "To generate a KC1 comment, I will analyze the passage you provide and identify relevant clues "
+        # prompt += "that can help me make a prediction about what might happen next in the story. This could involve "
+        # prompt += "looking for key words or phrases, making inferences based on the information provided, "
+        # prompt += "and using my knowledge of the topic to make an educated guess.\n\n"
+        # prompt += "For a KC1 comment, I will write a sentence or two that makes a prediction about what might happen "
+        # prompt += "next in the story, based on the information presented in the text. I will use language that is "
+        # prompt += "appropriate for the audience and may include emotional encouragement to keep the child engaged "
+        # prompt += "and interested in the story.\n\nOverall, a KC1 comment is a great way to help children develop "
+        # prompt += "their reading comprehension skills by encouraging them to make connections between different "
+        # prompt += "parts of the story and think critically about what might happen next."
